a73452ee75c4599435fa751f0b961a17fc45ae77
OmriOvadia8 <sirsone7@gmail.com>
2023-06-15 04:09:30 +0300

    Ability Spawn Upon Roll

--- Assets/_SM/Scripts/Game Logic/Managers/Ability/SDAbilityRoller.cs
+++ Assets/_SM/Scripts/Game Logic/Managers/Ability/SDAbilityRoller.cs
@@ -6,14 +6,11 @@ namespace SD_Ability
 {
     public class SDAbilityRoller : SDLogicMonoBehaviour
     {
-        public SDAbilityData RollSkill()
+        public SDAbilityData GetRandomAbility()
         {
             var abilities = SDAbilityDataManager.Abilities.AbilitiesInfo;
-
             var unlockedAbilities = abilities.Where(a => a.IsUnlocked).ToArray();
-
             int index = Random.Range(0, unlockedAbilities.Length);
-
             return unlockedAbilities[index];
         }
     }
--- Assets/_SM/Scripts/Game Logic/Managers/UI/SDAbilityRollingUI.cs
+++ Assets/_SM/Scripts/Game Logic/Managers/UI/SDAbilityRollingUI.cs
@@ -1,8 +1,8 @@
 using SD_Ability;
 using UnityEngine;
 using SD_GameLoad;
-using Sirenix.OdinInspector;
 using UnityEngine.UI;
+using System.Collections;
 
 namespace SD_UI
 {
@@ -10,18 +10,32 @@ namespace SD_UI
     {
         [SerializeField] SDAbilityRoller abilityRoller;
         [SerializeField] Image chosenAbilityIcon;
+        [SerializeField] SDAbilityAnimationController abilityAnimationController;
 
-        public void OnRoll()
+        public void OnRoll() => StartCoroutine(RollCoroutine());
+
+        private IEnumerator RollCoroutine()
         {
-            SDAbilityData chosenAbility = abilityRoller.RollSkill();
+            float rollDuration = 1.0f;
+            float elapsedTime = 0.0f;
+
+            while (elapsedTime < rollDuration)
+            {
+                SDAbilityData randomAbility = abilityRoller.GetRandomAbility();
+                ChosenAbilityIconChange(randomAbility);
+                yield return null;
+                elapsedTime += Time.deltaTime;
+            }
+
+            SDAbilityData chosenAbility = abilityRoller.GetRandomAbility();
             ChosenAbilityIconChange(chosenAbility);
+            abilityAnimationController.UseAbility(chosenAbility.AbilityName);
         }
 
-        private void ChosenAbilityIconChange(SDAbilityData chosenAbility)
+        private void ChosenAbilityIconChange(SDAbilityData abilityData)
         {
-            Sprite chosenAbilityIconSprite = chosenAbility.GetIcon();
-            chosenAbilityIcon.sprite = chosenAbilityIconSprite;
+            Sprite abilityIcon = abilityData.GetIcon();
+            chosenAbilityIcon.sprite = abilityIcon;
         }
-
     }
 }
